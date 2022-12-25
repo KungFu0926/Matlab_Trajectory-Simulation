228067b5650614a4aed4a41a3b73154aedeea828
ZhengKungFu <Nfu202208@gmail.com>
2022-12-25 17:06:45 +0800

    新增標示最大高度1.5~1.6區間，並刪除最大高度標示

--- DrawTrack.m
+++ DrawTrack.m
@@ -1,4 +1,4 @@
-function [Ax,Ay,MaxDis,MaxHeight,Maxcolumn,Maxrow,tem]=DrawTrack(HorizonDis,height)
+function [Ax,Ay,Maxcolumn,Maxrow]=DrawTrack(HorizonDis,height)
 Maxcolumn=min(size(height));%%size會得到column和row, min取最小值也就是column
 Maxrow=max(size(height));%%size會得到column和row, min取最小值也就是column
 Ax=zeros(Maxcolumn,Maxrow);
@@ -17,35 +17,46 @@ for i=1:1:Maxcolumn
         end
     end
 end
-MaxDis=zeros(1,Maxcolumn);
-MaxHeight=zeros(1,Maxcolumn);
-tem=zeros(1,Maxcolumn);%最高點發生的位置
-%找出最高點
-for i=1:1:Maxcolumn
-    for j=1:1:Maxrow-1
-        if Ay(i,j)<Ay(i,j+1)
-            MaxHeight(1,i)=Ay(i,j+1);
-            MaxDis(1,i)=Ax(i,j+1);
-            tem(1,i)=j+1;
-        else
-            MaxHeight(1,i)=Ay(i,j);
-            MaxDis(1,i)=Ax(i,j);
-            break;
-        end
 
-    end
-end
-
-
-
+%繪圖
 hold on
 for i=1:1:Maxcolumn
-    plot(Ax(i,:),Ay(i,:))
+    line=plot(Ax(i,:),Ay(i,:));
+    set(line,'linewidth',2);
 end
-plot(MaxDis(1,:),MaxHeight(1,:),'*b')
-legend("殺球","平抽球","低位高遠球","高位高遠球","近網吊球","軌道最高點")
-
+for i=1:1:Maxcolumn
+        for j=1:1:Maxrow-1
+            if Ay(i,j)>1.50 & Ay(i,j)<1.60
+                LimPoint=plot(Ax(i,j),Ay(i,j),'r.');
+                set(LimPoint,'MarkerSize',10);
+            end
+        end
+end
+%legend("殺球","平抽球","低位高遠球","高位高遠球","近網吊球","軌道最高點")
+legend("Y1","Y2","Y3","高度1.50~1.60")
 xlabel("水平距離");
 ylabel("垂直高度");
 hold off
 end
+
+
+
+% MaxDis=zeros(1,Maxcolumn);
+% MaxHeight=zeros(1,Maxcolumn);
+% tem=zeros(1,Maxcolumn);%最高點發生的位置
+% 找出最高點
+% for i=1:1:Maxcolumn
+%     for j=1:1:Maxrow-1
+%         if Ay(i,j)<Ay(i,j+1)
+%             MaxHeight(1,i)=Ay(i,j+1);
+%             MaxDis(1,i)=Ax(i,j+1);
+%             tem(1,i)=j+1;
+%         else
+%             MaxHeight(1,i)=Ay(i,j);
+%             MaxDis(1,i)=Ax(i,j);
+%             break;
+%         end
+% 
+%     end
+% end
+%plot(MaxDis(1,:),MaxHeight(1,:),'*b')
--- trackcal.m
+++ trackcal.m
@@ -1,6 +1,9 @@
 function [HorizonDis,height]=trackcal(Vi,deg,InitialPos)
-%[HorizonDis,height]=trackcal([80;50;50;60;40],[-10;0;50;40;30],[2;1.6;0.9;1.7;0.9])要用分號不能用逗號
-%殺球,平球,低位高遠球
+%[HorizonDis,height]=trackcal([80;50;50;60;30],[-10;0;50;40;8],[2;1.6;0.9;1.7;1.2])要用分號不能用逗號
+%殺球,平球,低位高遠球,高位高遠球,近網吊球
+%[HorizonDis,height]=trackcal([30;56;56],[30;30;60],[0;0;0])
+%legend("Y1","Y2","Y3","高度1.50~1.60")
+
 m=5.19;
 G=9.8;
 n=2;
@@ -17,7 +20,7 @@ k2=(Vxi.* G.*t+k1)./k1;
 HorizonDis=(k1./G).*log(k2);
 s1=atan(Vt./Vyi);
 s2=(G.*t./Vt)+s1;
-height=(Vt.^2./G)*log(abs(sin(s2)./sin(s1)))+InitialPos;
+height=(Vt.^2./G).*log(abs(sin(s2)./sin(s1)))+InitialPos;
 end
 
 
